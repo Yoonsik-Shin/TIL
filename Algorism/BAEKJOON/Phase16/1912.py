dda2fee7e638c7e8d69328d49d1a5dd2f5789d09
Yoonsik-Shin <sys25800994@gmail.com>
2022-08-29 22:09:34 +0900

    0829중간정리

--- Algorism/BAEKJOON/Phase16/1912.py
+++ Algorism/BAEKJOON/Phase16/1912.py
@@ -1,22 +1,13 @@
-# 못품
+def maxSubArray(nums):
+    total_max = nums[0]
+    cur_max = nums[0]
+
+    for i in range(1, len(nums)):
+        cur_max = max(nums[i] + cur_max, nums[i])
+        total_max = max(total_max, cur_max)
+
+    return total_max
 
 n = int(input())
-
-lst = list(map(int,input().split()))
-check_lst = [lst[0]]
-ans_max = -1e9
-
-for i in range(1,n):
-    
-    if len(check_lst) == 0:
-        check_lst.append(lst[i])
-        continue
-
-    check_lst.append(check_lst[-1]+lst[i])
-    
-    if check_lst[-1] < check_lst[-2]:
-        if check_lst[-2] > ans_max:
-            ans_max = check_lst[-2]
-        check_lst = []
-
-print(ans_max)
+numbers = list(map(int,input().split()))
+print(maxSubArray(numbers))
