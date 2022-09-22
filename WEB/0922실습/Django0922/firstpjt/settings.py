514e1f4b43924451c5fade08cd31685a10434369
Yoonsik-Shin <sys25800994@gmail.com>
2022-09-22 17:39:10 +0900

    django Secret Key 수정

--- WEB/0922실습/Django0922/firstpjt/settings.py
+++ WEB/0922실습/Django0922/firstpjt/settings.py
@@ -9,18 +9,33 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y5rl*q)uz9f1a_&v!wp1b#&g^42d-kr0h)%iy60k-vrx+%4t%+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
